671575ff03c7a3f69dbe7f146d723fe5d136e0a9
Asai Andrade <asaiandrade@gmail.com>
2019-12-23 21:47:54 -0800

    initial commit

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from bs4 import BeautifulSoup
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Auto reload working?!'
+
+@app.route('/lol')
+def main():
+    response = requests.get('https://watch.lolesports.com/schedule?leagues=all-star,lcs,lec,lck,lpl,lcs-academy,turkiye-sampiyonluk-ligi,cblol-brazil,lla,oce-opl,ljl-japan,worlds,msi,rift-rivals-na-eu,rift-rivals-kr-cn-lms-vn,league-of-legends-college-championship,european-masters')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print(soup.prettify())
+    return ''
